acc7cc810ae1217ea1a789bf82f8e0b1bddc04d9
Bryan Lo <blo@oandacorp.com>
2017-06-19 15:46:36 -0400

    stuff for pre/post hooks

--- mine/hi.scala
+++ mine/hi.scala
@@ -7,8 +7,18 @@ object MainDude {
 }
 
 class MainDude extends Actor with ActorLogging {
+
+	override def preStart() {
+		log.info("just started!")
+	}
+
+	override def postStop() {
+		log.info("just stopped..")
+	}
+
 	def receive: Receive = {
 		case "ok" => log.info("OKKKK")
+		case "no" => throw new Exception("nooooooo")
 	}
 }
 
@@ -18,8 +28,10 @@ object Hi {
 		try {
 			val dude = system.actorOf(MainDude.props(), "thedude")
 			dude ! "ok"
-			while (StdIn.readLine() == "") {
-				dude ! "ok"
+			var input = StdIn.readLine()
+			while (true) {
+				dude ! input
+				input = StdIn.readLine()
 			}
 		} finally {
 			system.terminate()
